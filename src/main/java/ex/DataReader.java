6c1b898251f8c5e99bd336bce16dd2537823b2f9
IFISOLUTION\nqkhai <khai.nguyenquang@ifisolution.com>
2020-10-14 17:38:13 +0700

    Commit data provider ver0.1

---
+++ src/main/java/ex/DataReader.java
@@ -0,0 +1,85 @@
+package ex;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+/*
+ * public class DataReader { public static Object[][] readExcel(String
+ * filePath,String fileName,String sheetName) throws IOException{ //Create an
+ * object of File class to open xlsx file File file = new File(filePath);
+ * //Create an object of FileInputStream class to read excel file
+ * FileInputStream inputStream = new FileInputStream(file); Workbook workbook =
+ * null; //Find the file extension by splitting file name in substring and
+ * getting only extension name String fileExtensionName =
+ * fileName.substring(fileName.indexOf(".")); //Check condition if the file is
+ * xlsx file if(fileExtensionName.equals(".xlsx")){ //If it is xlsx file then
+ * create object of XSSFWorkbook class workbook = new XSSFWorkbook(inputStream);
+ * } //Check condition if the file is xls file else
+ * if(fileExtensionName.equals(".xls")){ //If it is xls file then create object
+ * of HSSFWorkbook class workbook = new HSSFWorkbook(inputStream); } //Read
+ * sheet inside the workbook by its name Sheet a = workbook.getSheet(sheetName);
+ * //Find number of rows in excel file int rowCount =
+ * a.getLastRowNum()-a.getFirstRowNum(); //Create a loop over all the rows of
+ * excel file to read it for (int i = 0; i < rowCount+1; i++) { Row row =
+ * a.getRow(i); //Create a loop to print cell values in a row for (int j = 0; j
+ * < row.getLastCellNum(); j++) { //Print Excel data in console
+ * System.out.print(row.getCell(j).getStringCellValue()+"|| "); }
+ * System.out.println(); } return null; } //Main function is calling readExcel
+ * function to read data from excel file public static void
+ * main(String...strings) throws IOException{ //Prepare the path of excel file
+ * String filePath =
+ * System.getProperty("user.dir")+"\\src\\excelExportAndFileIO"; //Call read
+ * file method of the class to read data
+ * DataReader.readExcel(filePath,"data.xlsx","Sheet1"); } }
+ */
+
+public class DataReader {
+	public static Object[][] readExcel(String pathToExcel, String sheetName) throws IOException
+	{
+	    FileInputStream fileInputStream= new FileInputStream(pathToExcel); 
+	    XSSFWorkbook workbook = new XSSFWorkbook (fileInputStream); //get my workbook from xls (not working with xlsx)
+	    XSSFSheet worksheet=workbook.getSheet(sheetName);// get workbook
+	    DataFormatter formatter= new DataFormatter();
+        XSSFRow row=worksheet.getRow(0);     //get first row  
+     
+        int RowNum = worksheet.getPhysicalNumberOfRows();// number of Rows
+        int ColNum= row.getLastCellNum(); // number of Columns 
+         
+        Object Data[][]= new Object[RowNum-1][ColNum]; 
+         
+            for(int i=0; i<RowNum-1; i++) 
+            {  
+                row= worksheet.getRow(i+1);
+                 
+                for (int j=0; j<ColNum; j++) 
+                {
+                    if(row==null)
+                        Data[i][j]= "";
+                    else
+                    {
+                        XSSFCell cell= row.getCell(j);
+                        if(cell==null)
+                            Data[i][j]= ""; //if it get Null value it pass no data 
+                        else
+                        {
+                            String value=formatter.formatCellValue(cell); //Get cell value as string
+                            Data[i][j]=value;                         
+                        }
+                    }
+                }
+            }
+        workbook.close();
+        return Data;
+	}
+}
--- src/main/java/ex/NewTest.java
+++ src/main/java/ex/NewTest.java
@@ -2,31 +2,39 @@ package ex;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.Assert;
+
+import java.io.IOException;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class NewTest {
     private WebDriver driver;
-    
-  @Test
-  public void f() {
-      String baseUrl = "https://www.seleniumeasy.com/test/basic-first-form-demo.html";
+    String baseUrl = "https://www.seleniumeasy.com/test/basic-first-form-demo.html";
+    String InputMsgXPath = "//*[@id=\"user-message\"]";
+    String ShowMsgButtonXpath = "//*[@id=\"get-input\"]/button";
+    String MsgDisplayXpath = "//*[@id=\"display\"]";
+    String InputText = "hsdifeeekdue";
+  @DataProvider
+    public static Object[][] dataFromExcel() throws IOException{
+    	System.out.println("DataProvider:dataFromExcel");
+    	return DataReader.readExcel("data\\data.xls", "Sheet1");
+  }
+  @Test(groups={"easy"},dataProvider="dataFromExcel")
+  public void f(String inputData) {      
 	  driver.get(baseUrl);
-      String InputMsgXPath = "//*[@id=\"user-message\"]";
-      String ShowMsgButtonXpath = "//*[@id=\"get-input\"]/button";
-      String MsgDisplayXpath = "//*[@id=\"display\"]";
-      String InputText = "hsdifeeekdue";
 	  WebElement input = driver.findElement(By.xpath(InputMsgXPath));
       WebElement button = driver.findElement(By.xpath(ShowMsgButtonXpath));
-      input.sendKeys(InputText);
+      input.sendKeys(inputData);
       button.click();
       
       String result = driver.findElement(By.xpath(MsgDisplayXpath)).getText();
-      Assert.assertEquals(result,InputText); 	
+      Assert.assertEquals(result,inputData); 	
   }
   @BeforeTest
   public void beforeTest() {
