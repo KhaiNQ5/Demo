6754828418cfe807b1bd38bdb0cf87b22dae0016
IFISOLUTION\nqkhai <khai.nguyenquang@ifisolution.com>
2020-10-15 11:17:28 +0700

    added new case

--- src/main/java/ex/DataReader.java
+++ src/main/java/ex/DataReader.java
@@ -4,6 +4,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
@@ -48,27 +50,23 @@ public class DataReader {
 	public static Object[][] readExcel(String pathToExcel, String sheetName) throws IOException
 	{
 	    FileInputStream fileInputStream= new FileInputStream(pathToExcel); 
-	    XSSFWorkbook workbook = new XSSFWorkbook (fileInputStream); //get my workbook from xls (not working with xlsx)
-	    XSSFSheet worksheet=workbook.getSheet(sheetName);// get workbook
+	    HSSFWorkbook workbook = new HSSFWorkbook (fileInputStream); //get my workbook from xls (not working with xlsx)
+	    HSSFSheet worksheet=workbook.getSheet(sheetName);// get workbook
 	    DataFormatter formatter= new DataFormatter();
-        XSSFRow row=worksheet.getRow(0);     //get first row  
-     
+        HSSFRow row=worksheet.getRow(0);     //get first row  
         int RowNum = worksheet.getPhysicalNumberOfRows();// number of Rows
         int ColNum= row.getLastCellNum(); // number of Columns 
-         
-        Object Data[][]= new Object[RowNum-1][ColNum]; 
-         
+        Object Data[][]= new Object[RowNum-1][ColNum];          
             for(int i=0; i<RowNum-1; i++) 
             {  
-                row= worksheet.getRow(i+1);
-                 
+                row= worksheet.getRow(i+1);                
                 for (int j=0; j<ColNum; j++) 
                 {
                     if(row==null)
                         Data[i][j]= "";
                     else
                     {
-                        XSSFCell cell= row.getCell(j);
+                        HSSFCell cell= row.getCell(j);
                         if(cell==null)
                             Data[i][j]= ""; //if it get Null value it pass no data 
                         else
--- src/main/java/ex/NewTest.java
+++ src/main/java/ex/NewTest.java
@@ -7,26 +7,34 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.Assert;
-
 import java.io.IOException;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class NewTest {
-    private WebDriver driver;
+    public WebDriver driver;
     String baseUrl = "https://www.seleniumeasy.com/test/basic-first-form-demo.html";
     String InputMsgXPath = "//*[@id=\"user-message\"]";
     String ShowMsgButtonXpath = "//*[@id=\"get-input\"]/button";
     String MsgDisplayXpath = "//*[@id=\"display\"]";
-    String InputText = "hsdifeeekdue";
+    String InputAValue = "//*[@id=\"sum1\"]";
+    String InputBValue = "//*[@id=\"sum2\"]";
+    String GetResultButton = "//*[@id=\"gettotal\"]/button";
+    String Result = "//*[@id=\"displayvalue\"]";
+
   @DataProvider
     public static Object[][] dataFromExcel() throws IOException{
     	System.out.println("DataProvider:dataFromExcel");
-    	return DataReader.readExcel("data\\data.xls", "Sheet1");
+    	return DataReader.readExcel("data\\data.xls", "Sheet2");
   }
-  @Test(groups={"easy"},dataProvider="dataFromExcel")
-  public void f(String inputData) {      
+  @DataProvider
+  	public static Object[][] dataFromExcel2() throws IOException{
+  		System.out.println("DataProvider:dataFromExcel");
+  		return DataReader.readExcel("data\\data.xls", "Sheet1");
+}
+  @Test(groups={"test1", "asrx"}, dataProvider="dataFromExcel2")
+  public void f(String inputData) {    
+	  System.out.println("Testing testing 123");
 	  driver.get(baseUrl);
 	  WebElement input = driver.findElement(By.xpath(InputMsgXPath));
       WebElement button = driver.findElement(By.xpath(ShowMsgButtonXpath));
@@ -35,6 +43,21 @@ public class NewTest {
       
       String result = driver.findElement(By.xpath(MsgDisplayXpath)).getText();
       Assert.assertEquals(result,inputData); 	
+  }
+  @Test(groups={"no"},dataProvider="dataFromExcel")
+  public void f1(String inputInt1, String inputInt2) {    
+	  System.out.println("Testing testing 456");
+      driver.get(baseUrl);
+      WebElement input = driver.findElement(By.xpath(InputAValue));
+      WebElement input1 = driver.findElement(By.xpath(InputBValue));
+      WebElement button = driver.findElement(By.xpath(GetResultButton));
+      input.sendKeys(inputInt1);
+      input1.sendKeys(inputInt2);
+      button.click();
+      int inputtotal = Integer.parseInt(inputInt1) + Integer.parseInt(inputInt2);
+      String result = driver.findElement(By.xpath(Result)).getText();
+      int sum = Integer.parseInt(result);
+      Assert.assertTrue(inputtotal == sum);
   }
   @BeforeTest
   public void beforeTest() {
